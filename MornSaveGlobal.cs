555ddb0f3612f5bc6ba3a96048785bafe40aa306
matsufriends <pikapikan0526@gmail.com>
2026-01-10 19:40:24 +0900

    namespaceのリファクタ

--- MornSaveGlobal.cs
+++ MornSaveGlobal.cs
@@ -5,6 +5,7 @@ namespace MornLib
     [CreateAssetMenu(fileName = nameof(MornSaveGlobal), menuName = "Morn/" + nameof(MornSaveGlobal))]
     public sealed class MornSaveGlobal : MornGlobalBase<MornSaveGlobal>
     {
+        public override string ModuleName => "MornSave";
         [Tooltip("PlayerPrefsに保存する際のキー名")]
         public string CorePlayerPrefsKey = "SaveData";
         [Tooltip("セーブデータのファイル名（拡張子なし）")]
@@ -14,6 +15,5 @@ namespace MornLib
         [Tooltip("セーブデータを保存するディレクトリ名")]
         public string SaveDir = "Shared";
         /// <inheritdoc/>
-        protected override string ModuleName => "MornSave";
     }
 }
--- MornSaveManager.cs
+++ MornSaveManager.cs
@@ -1,6 +1,5 @@
 using System;
 using System.IO;
-using MornDebug;
 using UnityEngine;
 
 namespace MornLib
